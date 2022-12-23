ee42abeced9ff2c438c8ab864a6124328f0fb359
Tom Gregor <tgregor@ivsoftware.com>
2022-12-23 10:28:20 -0700

    prelim Ok

--- grid-view-double-click-00/MainForm.cs
+++ grid-view-double-click-00/MainForm.cs
@@ -1,8 +1,17 @@
 using System.ComponentModel;
+using static DevExpress.DataProcessing.InMemoryDataProcessor.AddSurrogateOperationAlgorithm;
+using System.ComponentModel.DataAnnotations;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.Utils;
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+using DevExpress.XtraBars.Docking;
 
 namespace grid_view_double_click_00
 {
     // https://docs.devexpress.com/WindowsForms/634/controls-and-libraries/data-grid/data-binding
+    // https://supportcenter.devexpress.com/ticket/details/q303121/gridcontrol-disable-edit-on-click-enable-edit-on-double-click
     public partial class MainForm : Form
     {
         public MainForm()
@@ -11,7 +20,23 @@ namespace grid_view_double_click_00
             gridControl.DataSource = Animals;
             Animals.Add(new Animal { Name = "Luna", Kind = Kind.Cat });
             Animals.Add(new Animal { Name = "Daisy", Kind = Kind.Dog});
+            gridControl.DoubleClick += onGridControlDoubleClick;
+            var view = (GridView)gridControl.MainView;
+            view.OptionsBehavior.Editable = false;
         }
+
+        private void onGridControlDoubleClick(object? sender, EventArgs e)
+        {
+            if((sender is GridControl control) && e is DXMouseEventArgs args) 
+            {
+                var hittest = (GridHitInfo)control.MainView.CalcHitInfo(args.Location);
+                // BTW don't block the click event to do this.
+                BeginInvoke(() => 
+                    MessageBox.Show(Animals[hittest.RowHandle].ToString())
+                );
+            }
+        }
+
         public BindingList<Animal> Animals { get; } = new BindingList<Animal>();
     }
     public enum Kind
@@ -22,8 +47,10 @@ namespace grid_view_double_click_00
     }
     public class Animal
     {
+        [Display(AutoGenerateField = false)]
         public string ID { get; set; } = Guid.NewGuid().ToString().Substring(0,8);
         public string? Name { get; set; }
         public Kind Kind { get; set; }
+        public override string ToString() => $"{Name}, {Kind}";
     }
 }
